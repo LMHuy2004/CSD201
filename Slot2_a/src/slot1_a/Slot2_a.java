42c18375f2d42133155b75631fd7cb13b78d333c
LMHuy2004 <l26m10h2004@gmail.com>
2025-04-14 18:34:20 +0700

    1

---
+++ Slot2_a/src/slot1_a/Slot2_a.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package slot1_a;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author l26m1
+ */
+public class Slot2_a {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ArrayList<Integer> list1 = new ArrayList<Integer>();
+        list1.add(5);
+        list1.add(10);
+        for (int i = 0; i < 100000; i++) {
+            list1.add(i);
+        }
+        for (int i = 0; i < 10; i++) {
+            System.out.println(list1.get(i));
+        }
+        
+        list1.remove(0);
+        
+        list1.remove(list1.size()-1);
+        
+        list1.add(0, -1);
+        
+        list1.add(list1.size()-1, -100);
+        
+        list1.indexOf(5); // Vị trí
+        
+        list1.get(list1.indexOf(5));
+        
+        //list1....
+        
+        System.out.println("Số lượng: "+ list1.size());
+    }
+}
