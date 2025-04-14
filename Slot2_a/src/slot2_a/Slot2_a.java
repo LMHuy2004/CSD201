f38fbfd1477e6ec3aea005d80a971b1d4f4820b2
LMHuy2004 <l26m10h2004@gmail.com>
2025-04-14 19:14:03 +0700

    1

--- Slot2_a/src/slot2_a/Slot2_a.java
+++ Slot2_a/src/slot2_a/Slot2_a.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package slot1_a;
+package slot2_a;
 
 import java.util.ArrayList;
 
@@ -17,30 +17,28 @@ public class Slot2_a {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        ArrayList<Integer> list1 = new ArrayList<Integer>();
-        list1.add(5);
-        list1.add(10);
-        for (int i = 0; i < 100000; i++) {
+        ArrayList<Integer> list1 = new ArrayList<Integer>();        
+        list1.add(27);
+        list1.add(12);
+        
+        for (int i = 0; i < 10; i++) {
             list1.add(i);
         }
         for (int i = 0; i < 10; i++) {
             System.out.println(list1.get(i));
         }
         
-        list1.remove(0);
-        
-        list1.remove(list1.size()-1);
-        
-        list1.add(0, -1);
-        
+//        list1.remove(0);
+//        list1.remove(list1.size()-1);
+
+        list1.add(2, -1);
         list1.add(list1.size()-1, -100);
         
         list1.indexOf(5); // Vị trí
-        
         list1.get(list1.indexOf(5));
         
-        //list1....
-        
-        System.out.println("Số lượng: "+ list1.size());
+        System.out.println("Số lượng: "+ list1+" || "+list1.size()
+                +"\nVị trí số: "+ list1.indexOf(5)
+                +"\n- "+ list1.get(list1.indexOf(9)));
     }
 }
