148475e4a6416ee81f030497494fa99fd8377f87
LMHuy2004 <l26m10h2004@gmail.com>
2025-04-16 10:02:18 +0700

    3

---
+++ Slot05_06_SLL/src/MyList.java
@@ -0,0 +1,222 @@
+
+import java.io.File;
+import java.io.RandomAccessFile;
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author l26m1
+ */
+public class MyList {
+
+    Node head, tail;
+    int size;
+
+    public MyList() {
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    }
+
+    public boolean isEmpty() {
+        return this.size == 0;
+    }
+
+    public void clear() {
+        this.head = null;
+        this.head = null;
+        this.size = 0;
+    }
+
+    void traverse() {
+        Node p = head;
+        while (p != null) {
+            System.out.print(p.info);
+            p = p.next;
+        }
+    }
+
+    void ftraverse(RandomAccessFile f) throws Exception {
+        Node p = head;
+        while (p != null) {
+            f.writeBytes(p.info + " ");
+            p = p.next;
+        }
+        f.writeBytes("\r\n"); // \r thục vào đầu dòng
+    }
+
+    void loadData(int k) { // k: là dòng thứ k trong file
+        String[] a = Lib.readLineToStrArray("data.txt", k);
+        int n = a.length;
+        for (int i = 0; i < n; i++) {
+            int number = Integer.parseInt(a[i]);
+            addLast(number);
+        }
+    }
+
+    void addFirst(int n) {
+        Node newNode = new Node(n);
+        if (head == null) {
+//            head = newNode;
+//            tail = newNode;
+            head = tail = newNode;
+        } else {
+            newNode.next = head;
+            head = newNode;
+        }
+        size++;
+    }
+
+    void addLast(int n) {
+        Node newNode = new Node(n);
+        if (head == null) {
+            head = tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = tail.next;
+        }
+        size++;
+    }
+
+    // f1: ham nay se goi ham addLast nhieu lan
+    // f1: ham nay se goi ham addLast nhieu lan
+    void f1() throws Exception {
+        clear();
+        loadData(0);
+        String fname = "f1.txt";
+        File g123 = new File(fname);
+        if (g123.exists()) {
+            g123.delete();
+        }
+        RandomAccessFile f = new RandomAccessFile(fname, "rw");
+        ftraverse(f);
+        f.close();
+    }
+
+    // f2: ham addFirst ==> du lieu nhap tu ban phim
+    void f2() throws Exception {
+        clear();
+        loadData(0);
+        String fname = "f2.txt";
+        File g123 = new File(fname);
+        if (g123.exists()) {
+            g123.delete();
+        }
+        RandomAccessFile f = new RandomAccessFile(fname, "rw");
+        //------------------------------------------------------------------------------------
+        //------ Start your code here---------------------------------------------------------
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a value to add first: ");
+        int value = sc.nextInt();
+        addFirst(value);
+        //------ End your code here-----------------------------------------------------------
+        //------------------------------------------------------------------------------------
+        ftraverse(f);
+        f.close();
+    }
+
+    // f3: ham addPos ==> them node vao vi tri thu k, trong do node moi va chi so k duoc nhap tu ban phim
+    void f3() throws Exception {
+        clear();
+        loadData(0);
+        String fname = "f3.txt";
+        File g123 = new File(fname);
+        if (g123.exists()) {
+            g123.delete();
+        }
+        RandomAccessFile f = new RandomAccessFile(fname, "rw");
+
+        //------------------------------------------------------------------------------------
+        //------ Start your code here---------------------------------------------------------
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a value to add: ");
+        int value = sc.nextInt();
+        System.out.print("Enter position to add (0 to \" + size + \"): ");
+        int k = sc.nextInt();
+
+        if (k == 0) {
+            addFirst(value);
+        } else if (k == size) {
+            addLast(value);
+        } else {
+            Node newNode = new Node(value);
+            Node p = head;
+            // Traverse to the node before position k
+            for (int i = 0; i < k-1; i++) {
+                p = p.next;
+            }
+            // Insert the new node
+            newNode.next = p.next;
+            p.next = newNode;
+            size++;
+        }
+        //------ End your code here-----------------------------------------------------------
+        //------------------------------------------------------------------------------------
+        ftraverse(f);
+        f.close();
+    }
+
+    // f4: removeFirst
+    void f4() throws Exception {
+        clear();
+        loadData(0);
+        String fname = "f4.txt";
+        File g123 = new File(fname);
+        if (g123.exists()) {
+            g123.delete();
+        }
+        RandomAccessFile f = new RandomAccessFile(fname, "rw");
+
+        //------------------------------------------------------------------------------------
+        //------ Start your code here---------------------------------------------------------
+        if(!isEmpty()){
+            head = head.next;
+            if(head == null){
+                tail = null;
+            }
+            size--;
+        }
+        //------ End your code here-----------------------------------------------------------
+        //------------------------------------------------------------------------------------
+        ftraverse(f);
+        f.close();
+    }
+
+    // f5: removeLast
+    void f5() throws Exception {
+        clear();
+        loadData(0);
+        String fname = "f5.txt";
+        File g123 = new File(fname);
+        if (g123.exists()) {
+            g123.delete();
+        }
+        RandomAccessFile f = new RandomAccessFile(fname, "rw");
+        //------------------------------------------------------------------------------------
+        //------ Start your code here---------------------------------------------------------
+        if(!isEmpty()){
+            if(size==1){
+                head = tail = null;
+                size = 0;
+            }else{
+                Node p = head;
+                // find the node before the tail
+                for (int i = 0; i < size-2; i++) {
+                    p = p.next;
+                }
+                tail = p;
+                p.next = null;
+                size--;
+            }
+        }
+        //------ End your code here-----------------------------------------------------------
+        //------------------------------------------------------------------------------------
+        ftraverse(f);
+        f.close();
+    }
+}
